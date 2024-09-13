11d61a5d715ced2d70d2a74b5027f10b2376e874
limoamosli <limo@msoe.edu>
2024-09-12 19:04:31 -0500

    finish Die and DieNotRolledException classes

--- src/username/Die.java
+++ src/username/Die.java
@@ -2,11 +2,37 @@
  * Course: CSC1020
  * Lab 2 - Exceptions
  * Die class
- * Name: FIXME
- * Last Updated: FIXME
+ * Name: Amos Li
+ * Last Updated: 9/12/2024
  */
 package username;
-
+/**
+ *The die class represents a die with a signed number of sides.
+ *It provides method to roll the die and provide the current value.
+ */
 public class Die {
+    private final int numSides;
+    private int currentValue = 0;
+    private final int minSide = 2;
+    private final int maxSide = 100;
 
+    Die(int numSides){
+        this.numSides = numSides;
+        if(numSides < minSide || numSides > maxSide){
+            throw new IllegalArgumentException("illegal number of sides: "+ numSides);
+        }
+    }
+    int getCurrentValue(){
+        roll();
+        if(currentValue ==0 || currentValue > numSides){
+            throw new DieNotRolledException("Die has not been rolled yet.");
+        }
+        int value = currentValue;
+        currentValue = 0;
+        return value;
+    }
+
+    private void roll(){
+        currentValue = (int)(Math.random()*numSides+1);
+    }
 }
--- src/username/DieNotRolledException.java
+++ src/username/DieNotRolledException.java
@@ -2,11 +2,17 @@
  * Course: CSC1020
  * Lab 2 - Exceptions
  * DieNotRolledException class
- * Name: FIXME
- * Last Updated: FIXME
+ * Name: Amos Li
+ * Last Updated: 9/12/2024
  */
 package username;
 
-public class DieNotRolledException  {
-
+/**
+ *This exception is throw when an attempt is made to use a die that have not been rolled
+ *This class extends @NumberFormatException to indicate this exception is a number format issue.
+ */
+public class DieNotRolledException extends NumberFormatException {
+    DieNotRolledException(String message){
+        super(message);
+    }
 }
